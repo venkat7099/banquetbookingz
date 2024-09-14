c9393b06ca39fec0aa157ee4cd1291574963b932
santosh81066 <34810098+santosh81066@users.noreply.github.com>
2024-09-14 10:11:49 +0530

    Update stackwidget.dart

--- lib/widgets/stackwidget.dart
+++ lib/widgets/stackwidget.dart
@@ -1,136 +1,146 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../providers/authprovider.dart';
+import '../providers/bottomnavigationbarprovider.dart';
+import '../providers/searchtextnotifier.dart';
 
 class StackWidget extends StatelessWidget {
   final String? hintText;
   final IconData? arrow;
   final String text;
   final VoidCallback? onTap;
-  final VoidCallback? tabarrow;
-  const StackWidget(
-      {super.key,
-      required this.hintText,
-      this.arrow,
-      required this.text,
-      this.onTap,
-      this.tabarrow});
+  final VoidCallback? tabArrow;
+  final TextEditingController? textEditingController;
+
+  const StackWidget({
+    super.key,
+    this.hintText,
+    this.arrow,
+    required this.text,
+    this.onTap,
+    this.tabArrow,
+    this.textEditingController,
+  });
 
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
-    return Consumer(builder: (context, ref, child) {
-      final _selectedIndex = ref.watch(pageIndexProvider);
-      return Stack(
-        children: [
-          Column(
-            children: [
-              Container(
-                alignment: Alignment.center,
-                height: screenHeight * 0.25,
-                width: screenWidth,
-                color: Color(0xFFf5f5f5),
-              ),
-            ],
-          ),
-          Positioned(
-            child: Stack(
+
+    return Consumer(
+      builder: (context, ref, child) {
+        final _selectedIndex = ref.watch(pageIndexProvider);
+        final _usertype = ref.watch(authProvider);
+
+        return Stack(
+          children: [
+            Column(
               children: [
                 Container(
-                  height: screenHeight * 0.22,
-                  decoration: BoxDecoration(
-                    color: theme.primaryColor,
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(15),
-                      bottomRight: Radius.circular(15),
-                    ),
-                  ),
+                  alignment: Alignment.center,
+                  height: screenHeight * 0.10,
+                  width: screenWidth,
+                  color: const Color(0xFFFFFFFF), // Set background color to white
                 ),
-                Positioned(
-                    top: 50,
-                    left: 20,
-                    child: Row(
-                      children: [
-                        Consumer(builder: (context, ref, child) {
-                          return IconButton(
-                              onPressed: tabarrow,
-                              icon: Icon(
-                                arrow,
-                                color: Colors.white,
-                              ));
-                        }),
-                        Text(
-                          text,
-                          style: TextStyle(color: Colors.white, fontSize: 25),
-                        ),
-                      ],
-                    )),
-                // The search icon
-                Positioned(
-                    top: 50,
-                    right: 20,
-                    child: InkWell(
-                      onTap: onTap,
-                      child: _selectedIndex == 2
-                          ? Icon(
-                              Icons.add_circle_outline_rounded,
-                              size: 50,
-                              color: Colors.white,
-                            )
-                          : Stack(
-                              alignment: Alignment.bottomRight,
-                              children: <Widget>[
-                                Icon(
-                                  Icons.account_circle,
-                                  color: Colors.white,
-                                  size: 50.0,
-                                ),
-                                Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.purple,
-                                    shape: BoxShape.circle,
-                                  ),
-                                  child: Icon(
-                                    Icons.verified_user,
-                                    color: Colors.white,
-                                    size: 20.0,
-                                  ),
-                                ),
-                              ],
-                            ),
-                    )),
               ],
             ),
-          ),
-          Positioned(
-            bottom: 0,
-            left: 10,
-            right: 10,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                  horizontal: 20.0), // Add horizontal padding if needed
-              child: SizedBox(
-                width: screenWidth * 0.85,
-                child: TextField(
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    hintText: hintText,
-                    prefixIcon: Icon(Icons.search, color: Color(0xff6418c3)),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30.0),
-                      borderSide: BorderSide.none,
+            Positioned(
+              child: Stack(
+                children: [
+                  Container(
+                    height: screenHeight * 0.22,
+                    decoration: BoxDecoration(
+                      color: theme.primaryColor,
+                      borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(15),
+                        bottomRight: Radius.circular(15),
+                      ),
                     ),
-                    contentPadding:
-                        EdgeInsets.symmetric(horizontal: 20.0, vertical: 16.0),
                   ),
-                ),
+                  Positioned(
+                    top: 50,
+                    left: 20,
+                    child: Text(
+                      text,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                      ),
+                    ),
+                  ),
+                  _selectedIndex != 3 && _usertype.usertype=='a'
+                      ? Positioned(
+                          top: 50,
+                          right: 20,
+                          child: InkWell(
+                            onTap: onTap,
+                            child: _selectedIndex == 2
+                                ? const Icon(
+                                    Icons.add_circle_outline_rounded,
+                                    size: 50,
+                                    color: Colors.white,
+                                  )
+                                : Stack(
+                                    alignment: Alignment.bottomRight,
+                                    children: <Widget>[
+                                      const Icon(
+                                        Icons.account_circle,
+                                        color: Colors.white,
+                                        size: 50.0,
+                                      ),
+                                      Container(
+                                        decoration: const BoxDecoration(
+                                          color: Colors.purple,
+                                          shape: BoxShape.circle,
+                                        ),
+                                        child: const Icon(
+                                          Icons.local_police_sharp,
+                                          color: Colors.white,
+                                          size: 20.0,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                          ),
+                        )
+                      : Container(),
+                  Positioned(
+                    bottom: 0,
+                    left: 10,
+                    right: 10,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: SizedBox(
+                        width: screenWidth * 0.85,
+                        child: TextField(
+                          controller: textEditingController,
+                          onChanged: (value) {
+                            ref.read(searchTextProvider.notifier).setSearchText(value);
+                            print('Search text updated: $value'); // Debug print
+                          },
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white,
+                            hintText: hintText,
+                            prefixIcon: Icon(Icons.search, color: Color(0xff6418c3)),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                              borderSide: BorderSide.none,
+                            ),
+                            contentPadding: EdgeInsets.symmetric(
+                                horizontal: 20.0, vertical: 16.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-          )
-        ],
-      );
-    });
+          ],
+        );
+      },
+    );
   }
 }

f5529e9ae770aa4fcfb70c3f45a1cdc8786eb030
Muzammil <md.muzammil939@gmail.com>
2024-12-12 21:27:29 +0530

    add_subscription

--- lib/views/addsubscriber.dart
+++ lib/views/addsubscriber.dart
@@ -1,17 +1,93 @@
-import 'package:banquetbookingz/providers/loader.dart';
-import 'package:banquetbookingz/providers/selectionmodal.dart';
-import 'package:banquetbookingz/providers/subcsribersprovider.dart';
-import 'package:banquetbookingz/widgets/customelevatedbutton.dart';
-import 'package:banquetbookingz/widgets/customtextfield.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class AddSubscriber extends ConsumerWidget {
+import '../providers/authprovider.dart';
+import '../providers/loader.dart';
+
+class AddSubscriber extends ConsumerStatefulWidget {
   const AddSubscriber({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<AddSubscriber> createState() => _AddSubscriberState();
+}
+
+class _AddSubscriberState extends ConsumerState<AddSubscriber> {
+  // Define controllers as instance variables
+  final TextEditingController planController = TextEditingController();
+  final TextEditingController frequencyController = TextEditingController();
+  final TextEditingController subPlanController = TextEditingController();
+  final TextEditingController bookingsController = TextEditingController();
+  final TextEditingController pricingController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Dispose of controllers to prevent memory leaks
+    planController.dispose();
+    frequencyController.dispose();
+    subPlanController.dispose();
+    bookingsController.dispose();
+    pricingController.dispose();
+    super.dispose();
+  }
+
+  Future<void> postSubscriptionDetails({
+    required String userId,
+    required String planName,
+    required String frequency,
+    required String subPlanName,
+    required String numBookings,
+    required String price,
+    required WidgetRef ref,
+  }) async {
+    final url = Uri.parse('http://www.gocodedesigners.com/bbaddplan');
+    ref.read(loadingProvider.notifier).state = true;
+
+    // Log the details before making the request
+    print('Posting subscription details:');
+    print({
+      'user_id': userId,
+      'plan_name': planName,
+      'frequency': frequency,
+      'sub_plan_name': subPlanName,
+      'num_bookings': numBookings,
+      'price': price,
+    });
+
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode({
+          'user_id': userId,
+          'plan_name': planName,
+          'frequency': frequency,
+          'sub_plan_name': subPlanName,
+          'num_bookings': numBookings,
+          'price': price,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        print('Subscription added successfully: ${response.body}');
+      } else {
+        print('Failed to add subscription: ${response.body}');
+      }
+    } catch (e) {
+      print('Error occurred: $e');
+    } finally {
+      ref.read(loadingProvider.notifier).state = false;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
+    final currentUser = ref.watch(authProvider);
+    final userId = currentUser.data?.userId ?? '';
 
     return Scaffold(
       body: Container(
@@ -45,248 +121,96 @@ class AddSubscriber extends ConsumerWidget {
                     style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                   ),
                   const SizedBox(height: 18),
-                  const Text(
-                    "Plan",
-                    style: TextStyle(
-                      fontSize: 18,
-                    ),
-                  ),
+                  const Text("Plan", style: TextStyle(fontSize: 18)),
                   const SizedBox(height: 8),
-                  Consumer(builder: (context, ref, child) {
-                    final controller =
-                        ref.watch(selectionModelProvider.notifier);
-                    return CustomTextFormField(
-                      width: screenWidth * 0.8,
-                      keyBoardType: TextInputType.text,
+                  TextFormField(
+                    controller: planController,
+                    keyboardType: TextInputType.text,
+                    decoration: const InputDecoration(
                       hintText: 'Plan',
-                      onChanged: (newValue) {
-                        controller.updateSubname(newValue);
-                      },
-                    );
-                  }),
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
                   const SizedBox(height: 10),
-                  const Text(
-                    "Frequency",
-                    style: TextStyle(
-                      fontSize: 18,
+                  const Text("Frequency", style: TextStyle(fontSize: 18)),
+                  const SizedBox(height: 8),
+                  TextFormField(
+                    controller: frequencyController,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      hintText: 'Frequency',
+                      border: OutlineInputBorder(),
                     ),
                   ),
-                  const SizedBox(height: 8),
-                
-                  Consumer(builder: (context, ref, child) {
-                        final controller =
-                            ref.watch(selectionModelProvider.notifier);
-                        return CustomTextFormField(
-                          width: screenWidth * 0.8,
-                          keyBoardType: TextInputType.number,
-                          hintText: 'Frequency',
-                          onChanged: (newValue) {
-                            controller.updateMonthlyP(newValue);
-                          },
-                        );
-                  }),
-                 const SizedBox(height: 10),
-                  const Text(
-                    "Subplan",
-                    style: TextStyle(
-                      fontSize: 18,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Consumer(builder: (context, ref, child) {
-                        
-                        final controller =
-                            ref.watch(selectionModelProvider.notifier);
-                        return CustomTextFormField(
-                          width: screenWidth * 0.8,
-                          keyBoardType: TextInputType.number,
-                          hintText: 'sub-plan',
-                          onChanged: (newValue) {
-                            controller.updateFrequency(newValue);
-                          },
-                        );   
-                 }),
-                    
                   const SizedBox(height: 10),
-                  const Text(
-                    "Booking",
-                    style: TextStyle(
-                      fontSize: 18,
+                  const Text("Sub-plan", style: TextStyle(fontSize: 18)),
+                  const SizedBox(height: 8),
+                  TextFormField(
+                    controller: subPlanController,
+                    keyboardType: TextInputType.text,
+                    decoration: const InputDecoration(
+                      hintText: 'Sub-plan',
+                      border: OutlineInputBorder(),
                     ),
                   ),
+                  const SizedBox(height: 10),
+                  const Text("Bookings", style: TextStyle(fontSize: 18)),
                   const SizedBox(height: 8),
-                  Consumer(builder: (context, ref, child) {
-                    final controller =
-                        ref.watch(selectionModelProvider.notifier);
-                    return CustomTextFormField(
-                      width: screenWidth * 0.8,
-                      keyBoardType: TextInputType.number,
+                  TextFormField(
+                    controller: bookingsController,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
                       hintText: 'Bookings',
-                      onChanged: (newValue) {
-                        controller.updateQuaterlyP(newValue);
-                      },
-                    );
-                  }),
-                  const SizedBox(height: 10),
-                  const Text(
-                    "Pricing",
-                    style: TextStyle(
-                      fontSize: 18,
+                      border: OutlineInputBorder(),
                     ),
                   ),
+                  const SizedBox(height: 10),
+                  const Text("Pricing", style: TextStyle(fontSize: 18)),
                   const SizedBox(height: 8),
-                  Consumer(builder: (context, ref, child) {
-                    final controller =
-                        ref.watch(selectionModelProvider.notifier);
-                    return CustomTextFormField(
-                      width: screenWidth * 0.8,
-                      keyBoardType: TextInputType.number,
+                  TextFormField(
+                    controller: pricingController,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
                       hintText: 'Pricing',
-                      onChanged: (newValue) {
-                        controller.updateAnnualP(newValue);
-                      },
-                    );
-                  }),
-                  const SizedBox(height: 12),
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
                 ],
               ),
               const SizedBox(height: 20),
               Consumer(builder: (context, ref, child) {
-                final selection = ref.watch(selectionModelProvider);
                 final loading = ref.watch(loadingProvider);
-                return CustomElevatedButton(
-                  text: "Add Subscriber",
-                  borderRadius: 10,
-                  foreGroundColor: Colors.white,
-                  width: double.infinity,
-                  backGroundColor: const Color(0XFF6418C3),
-                  isLoading: loading,
+
+                return ElevatedButton(
                   onPressed: loading
                       ? null
                       : () async {
-                          // Combine the frequency text and dropdown value
-                          String combinedFrequency = selection.monthlyP.text +
-                              selection.selectedFrequency;
+                          await postSubscriptionDetails(
+                            userId: '$userId',
+                            planName: planController.text,
+                            frequency: frequencyController.text,
+                            subPlanName: subPlanController.text,
+                            numBookings: bookingsController.text,
+                            price: pricingController.text,
+                            ref: ref,
+                          );
 
-                          // Validation check to prevent zero values
-                          if (selection.subName.text.isEmpty ||
-                              selection.annualP.text == '0' ||
-                              selection.quaterlyP.text == '0' ||
-                              combinedFrequency == '0monthly' ||
-                              combinedFrequency == '0quaterly' ||
-                              combinedFrequency == '0annually') {
-                            showDialog(
-                              context: context,
-                              builder: (context) {
-                                return const AlertDialog(
-                                  title: Text('Validation Error'),
-                                  content: Text(
-                                      'Please ensure all fields are filled and none of the values are zero.'),
-                                  actions: <Widget>[
-                                    TextButton(
-                                      onPressed: null,
-                                      child: Text('OK'),
-                                    ),
-                                  ],
-                                );
-                              },
-                            );
-                            return;
-                          }
-
-                          final SubscriberResult result = await ref
-                              .read(subscribersProvider.notifier)
-                              .addSubscriber(
-                                selection.subName.text,
-                                selection.annualP.text,
-                                selection.quaterlyP.text,
-                                combinedFrequency,
-                                ref,
-                              );
-                          if (result.statusCode == 201) {
-                            showDialog(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return Dialog(
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(20.0),
-                                  ),
-                                  child: Container(
-                                    padding: const EdgeInsets.all(20),
-                                    width: double.infinity,
-                                    decoration: const BoxDecoration(
-                                      shape: BoxShape.rectangle,
-                                      borderRadius:
-                                          BorderRadius.all(Radius.circular(20)),
-                                    ),
-                                    child: Column(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: <Widget>[
-                                        const Icon(Icons.check_circle,
-                                            size: 50, color: Color(0XFF6418C3)),
-                                        const SizedBox(height: 15),
-                                        const Text(
-                                          'Subscription has been successfully added as a user.',
-                                          textAlign: TextAlign.center,
-                                          style: TextStyle(
-                                            fontSize: 16,
-                                          ),
-                                        ),
-                                        const SizedBox(height: 15),
-                                        const Text(
-                                          'Login details have been mailed to the user.',
-                                          textAlign: TextAlign.center,
-                                          style: TextStyle(
-                                            color: Colors.grey,
-                                            fontSize: 14,
-                                          ),
-                                        ),
-                                        const SizedBox(height: 20),
-                                        Consumer(
-                                            builder: (context, ref, child) {
-                                          final addUser = ref.watch(
-                                              selectionModelProvider.notifier);
-                                          return CustomElevatedButton(
-                                            text: "OK",
-                                            borderRadius: 20,
-                                            width: 100,
-                                            foreGroundColor: Colors.white,
-                                            backGroundColor:
-                                                const Color(0XFF6418C3),
-                                            onPressed: () {
-                                              addUser.toggleAddUser(false);
-                                              Navigator.of(context)
-                                                  .pushNamed("dashboard");
-                                            },
-                                          );
-                                        })
-                                      ],
-                                    ),
-                                  ),
-                                );
-                              },
-                            );
-                          } else if (result.statusCode == 400) {
-                            showDialog(
-                              context: context,
-                              builder: (context) {
-                                return AlertDialog(
-                                  title: const Text('Login Error'),
-                                  content: Text(result.errorMessage ??
-                                      'An unknown error occurred.'),
-                                  actions: <Widget>[
-                                    TextButton(
-                                      child: const Text('OK'),
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                      },
-                                    ),
-                                  ],
-                                );
-                              },
-                            );
-                          }
+                          // Clear text fields after submission
+                          planController.clear();
+                          frequencyController.clear();
+                          subPlanController.clear();
+                          bookingsController.clear();
+                          pricingController.clear();
                         },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0XFF6418C3),
+                    foregroundColor: Colors.white,
+                    padding: const EdgeInsets.symmetric(vertical: 15),
+                    minimumSize: Size(double.infinity, 50),
+                  ),
+                  child: loading
+                      ? const CircularProgressIndicator(color: Colors.white)
+                      : const Text("Add Subscriber"),
                 );
               }),
             ],
